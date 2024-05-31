9ff474d93d4c65030d4ecb8bc8f595ba413b520d
xnewL <yagodiezdonoso@gmail.com>
2024-05-31 11:26:15 +0900

    Looks to be training, leftoverFiles

--- buildTrainValidation.py
+++ buildTrainValidation.py
@@ -1,13 +1,126 @@
 """
-File to build training and Validation datasets 
+File to build training and Validation datasets
 For Kanji Detection using deep learning
 """
+from imageUtils import sliding_window,read_Color_Image,read_Binary_Mask
+import os
+import cv2
+import sys
+from random import randint
+from pathlib import Path
 
-def buildTrainValid(imageFolder,maskFolder,perc):
+def boxesFromMask(img, cl = 0, yoloFormat = True):
     """
-        Receives a folder with images 
+    Return a list of box coordinates
+    From a binary image
+
+    Parameters
+    ----------
+    im : ndarray
+        taget image
+
+    Returns
+    -------
+    list: list of box coordinater
+    """
+    # void small regions
+    threshold = 500
+
+    # Binarize, just in case
+    img[img<=10] = 0
+    img[img>10] = 255
+    _, _, stats, centroids = cv2.connectedComponentsWithStats(255-img)
+
+    out = []
+    # Avoid first centroid, unbounded component
+    for j in range(1,len(centroids)):
+        if yoloFormat:
+            if stats[j][4] > threshold:
+                h, w = img.shape
+                bw = stats[j][2]
+                bh = stats[j][3]
+                cx = stats[j][0] + bw/2
+                cy = stats[j][1] + bh/2
+                # normalize and append
+                out.append((cl,cx/w,cy/h,bw/w,bh/h))
+        else:
+            raise Exception("UNSUPORTED BOX FORMAT")
+    return out
+
+def sliceAndBox(im,mask,slice):
+    """
+    Given image and mask, slice them
+    output image slices and mask slice
+    and text files
+    """
+    out = []
+    wSize = (slice,slice)
+    for (x, y, window) in sliding_window(im, stepSize = slice, windowSize = wSize ):
+        boxList = []
+        # get mask window
+        maskW = mask[y:y + wSize[1], x:x + wSize[0]]
+        # The mask was already binarized
+        # compute box coords
+        coords = boxesFromMask(maskW)
+        # add window, mask window and boxlist
+        out.append(("x"+str(x)+"y"+str(y),window,maskW,coords))
+    return out
+
+def boxCoordsToFile(file,boxC):
+    """
+        Receive a list of tuples
+        with bounding boxes
+        and write it to file
+
+    """
+    def writeTuple(tup):
+        c,px,py,w,h = tup
+        f.write(str(c)+" "+str(px)+" "+str(py)+" "+str(w)+" "+str(h)+"\n")
+
+    with open(file, 'a') as f:
+        list(map( writeTuple, boxC))
+
+
+def buildTrainValid(imageFolder, maskFolder, slice, outTrain, outVal, perc):
+    """
+        Receives a folder with images
         And another of masks
-        CAREFUL! name correspondences betwee
+        CAREFUL! name correspondences between
         mask and image files
     """
-    for 
+    # create output directories if they do not exist
+    #for d in [os.path.join(outTrain,"images"),os.path.join(outTrain,"masks"),
+    #os.path.join(outTrain,"labels"),os.path.join(outVal,"images"),
+    #os.path.join(outVal,"masks"),os.path.join(outVal,"labels")]:
+    #    Path(d).mkdir(parents=True, exist_ok=True)
+    for d in [os.path.join(outTrain,"images"),os.path.join(outTrain,"labels"),
+    os.path.join(outVal,"images"),os.path.join(outVal,"labels")]:
+        Path(d).mkdir(parents=True, exist_ok=True)
+
+    for dirpath, dnames, fnames in os.walk(maskFolder):
+        for f in fnames:
+            # read mask and image, everyone is binary
+            mask = read_Binary_Mask(os.path.join(maskFolder,f))
+            imageName = f[2:-6]+".tif_resultat_noiseRemoval.tif"
+            im = read_Binary_Mask(os.path.join(imageFolder,imageName))
+
+            # Masks are not the perfect size, reshape
+            mask = cv2.resize(mask, (im.shape[1],im.shape[0]))
+
+            # binarize mask
+            mask[mask<10] = 0
+            mask[mask>10] = 255
+
+            # call slice and box
+            sIML = sliceAndBox(im,mask,slice)
+
+            # store results (make up names)
+            for suffix,i,m,l in sIML:
+                newFileName = f[2:-6]+suffix
+
+                # randomDraw train/val
+                outDir = outTrain if randint(1,100) < perc else outVal
+                # store files including text file
+                cv2.imwrite(os.path.join(outDir,"images",newFileName+".png"),i)
+                #cv2.imwrite(os.path.join(outDir,"masks",newFileName+"MASK.png"),m)
+                boxCoordsToFile(os.path.join(outDir,"labels",newFileName+".txt"),l)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 """
    File to call the control the flow
    of an application to detect Kanji
-   In Wasan documents 
+   In Wasan documents
    using Deep Learning networks
 """
 
 import configparser
 import sys
+import yaml
+
+from buildTrainValidation import buildTrainValid
+from train import train_YOLO
 
 def read_config(filename):
     conf = configparser.ConfigParser()
@@ -19,16 +23,34 @@ def read_config(filename):
     res_dict["Test"] = conf[section].get('test') == "yes"
     res_dict["Post"] = conf[section].get('postprocess') == "yes"
 
-    section = 'TRAIN'
-    if res_dict["Prep"] or res_dict["Train"]:
+    section = 'PREP'
+    if res_dict["Prep"] :
         res_dict["Train_input_dir_images"] = conf[section].get('trainSourceImages')
         res_dict["Train_input_dir_masks"] = conf[section].get('trainSourceMasks')
+
+    section = 'TRAIN'
+    if res_dict["Prep"] or res_dict["Train"]:
+        res_dict["slice"] = int(conf[section].get('sliceSize'))
+
+        res_dict["TV_dir"] = conf[section].get('tVDir')
         res_dict["Train_dir"] = conf[section].get('trainDir')
         res_dict["Valid_dir"] = conf[section].get('validDir')
-        res_dict["Train_Perc"] = conf[section].get('trainPercentage')
+        res_dict["Train_Perc"] = int(conf[section].get('trainPercentage'))
+
+        res_dict["Train_res"] = conf[section].get('trainResFolder')
+        res_dict["Valid_res"] = conf[section].get('valResFolder')
+        res_dict["ep"] = int(conf[section].get('epochs'))
 
     return res_dict
 
+def makeTrainYAML(conf, fileName = 'trainAUTO.yaml'):
+    """
+    Function to write a yaml file
+    """
+    data = { "names": {0: 'Kanji'}, "path": conf["TV_dir"], "train": conf["Train_dir"],
+            "val": conf["Valid_dir"] }
+    with open(fileName, 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
 
 
 def main(fName):
@@ -39,6 +61,16 @@ def main(fName):
     conf = read_config(fName)
     print(conf)
 
+    # Do whatever needs to be done
+    if conf["Prep"]:
+        buildTrainValid(conf["Train_input_dir_images"],
+        conf["Train_input_dir_masks"],conf["slice"],conf["Train_dir"],
+        conf["Valid_dir"],conf["Train_Perc"])
+
+    if conf["Train"]:
+        yamlTrainFile = "trainAUTO.yaml"
+        makeTrainYAML(conf,yamlTrainFile)
+        train_YOLO(conf,yamlTrainFile)
 
 if __name__ == "__main__":
 
