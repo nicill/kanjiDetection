4d951719ce96a2bbbb96648efdaaaf9b7c340655
nicill <yagodiezdonoso@gmail.com>
2024-10-01 11:04:15 +0900

    build dataset with threshold and no hiragana from koutenshiki

--- kanjiUtils.py
+++ kanjiUtils.py
@@ -5,6 +5,7 @@ from math import sqrt
 import os
 from pathlib import Path
 import shutil
+from collections import defaultdict
 
 def gatherCharacters(source,dest):
     """
@@ -18,6 +19,40 @@ def gatherCharacters(source,dest):
         path = root.split(os.sep)
         if len(path) > 1 and path[-2] == "characters":
             shutil.copytree(root, os.path.join(dest,path[-1]),dirs_exist_ok=True)
+
+
+def makeKoutenshiki(source,dest,th = 9):
+    """
+        Copy folders not containing hiragana and 
+        with at least th files 
+    """
+    codeDict = defaultdict(lambda:0)
+    # create dict with codes
+    avoid_list = [
+                    'U+{:04X}'.format(i)
+                    for i in range(0x3040, 0x30A0)
+                ]
+
+    for root, dirs, files in os.walk("."):
+        path = root.split(os.sep)
+
+        for f in files:
+            if len(path) > 1 and path[-2] == "characters":
+                code = path[-1]
+                if code not in avoid_list:
+                    codeDict[code]+=1
+                #else:
+                #    print("avoiding "+code)
+            #if int(3040   int(code) != "30" and more files than th                
+            #    shutil.copytree(root, os.path.join(dest,path[-1]),dirs_exist_ok=True)
+    #print(codeDict)
+    # now only copy those with at least th repetitions as shown in codeDict
+    for k,v in codeDict.items():
+        if v > th:
+            print("copy "+k+" to "+str(os.path.join(dest,k))+ "because of having "+str(v))    
+            shutil.copytree(os.path.join(source,k), os.path.join(dest,k),dirs_exist_ok=True)
+
+
 
 def selectKanjiFromFile(inFile,inFolder,outFolder,etl = False):
     """
@@ -56,4 +91,6 @@ def selectKanjiFromFile(inFile,inFolder,outFolder,etl = False):
 
 
 if __name__ == '__main__':
-    print(selectKanjiFromFile(sys.argv[1],sys.argv[2],sys.argv[3],etl = True))
+    #print(selectKanjiFromFile(sys.argv[1],sys.argv[2],sys.argv[3],etl = True))
+    #gatherCharacters(sys.argv[1],sys.argv[2])
+    makeKoutenshiki(sys.argv[1],sys.argv[2])
